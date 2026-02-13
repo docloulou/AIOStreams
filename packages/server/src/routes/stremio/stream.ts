fdfa9c9f3a69357a60165c5eeee71794a5eb1e13
docloulou <lucas.zerbib@gmail.com>
2026-02-13 22:08:54 +0100

    Update stream.ts

--- packages/server/src/routes/stremio/stream.ts
+++ packages/server/src/routes/stremio/stream.ts
@@ -53,7 +53,7 @@ function generateStrmFilename(
   if (type === 'series' && parsedId?.season && parsedId?.episode) {
     const season = String(parsedId.season).padStart(2, '0');
     const episode = String(parsedId.episode).padStart(2, '0');
-    baseName += ` s${season}.e${episode}`;
+    baseName += ` S${season}E${episode}`;
   }
 
   return `${baseName}.strm`;
